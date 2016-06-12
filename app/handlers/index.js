d0a9401b06631273b63912d4118817f2131aac05
Santhosh Nageshwar <santhoshbabun@gmail.com>
2016-06-12 00:01:49 -0700

    changing port and logging request

--- app/handlers/index.js
+++ app/handlers/index.js
@@ -4,6 +4,7 @@ var Hoek = require('hoek');
 
 module.exports = {
     test: function(request, reply) {
+        console.log("v1/test request payload is " + request.payload);
         reply(request.payload);
     },
     api: function(request, reply) {
